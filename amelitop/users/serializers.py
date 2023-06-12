a3b3b21e007932391243c451f9f1bdf44d4a5aa5
Stanislav Veriho <sveriho@gmail.com>
2023-06-12 22:23:31 +0300

    Provide ID in API response

--- amelitop/users/serializers.py
+++ amelitop/users/serializers.py
@@ -13,7 +13,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name', 'last_name', 'profile')
+        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'profile')
+        read_only_fields = ('id',)
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
