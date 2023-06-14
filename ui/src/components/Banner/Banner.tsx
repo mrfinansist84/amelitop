7d6b9275663bf3849504be07b5473e7954b5c15f
mrfinansist84 <50926954+mrfinansist84@users.noreply.github.com>
2023-06-14 22:14:30 +0300

    User_management: Added loader and banner (#39)

--- ui/src/components/Banner/Banner.tsx
+++ ui/src/components/Banner/Banner.tsx
@@ -1,17 +1,56 @@
-import * as React from 'react'
-import './Banner.scss'
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import * as React from 'react';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import { BANNER_MESSAGE_STATUSES } from './constants';
+import './Banner.scss';
 
-
-interface IProps {
-  name: string
+interface Props {
+  show: boolean;
+  closeBanner: (flag: boolean) => void;
+  autoHide: boolean;
+  message: string;
+  status: string;
 }
 
-export default class Banner extends React.Component<IProps> {
-  render (): React.ReactNode {
-    return (
-      <div className="banner">
-        <span className="banner__text">Hello {this.props.name}!</span>
-      </div>
-    )
-  }
+export const Banner: React.FC<Props> = (props) => {
+  const getDataCorrespondingStatus = () => {
+    const info = BANNER_MESSAGE_STATUSES[props.status];
+    return {
+      name: info?.name,
+      bannerClass: info?.bannerClassName,
+      iconClass: info?.iconClassName
+    }
+  };
+
+  const getAutoHideStatus = () => props.autoHide || props.status === 'SUCCESS';
+
+  return (
+      <ToastContainer
+        className="p-3"
+        position="top-end"
+        style={{ zIndex: 1 }}
+    >
+      <Toast
+        onClose={() => props.closeBanner(false)}
+        show={props.show}
+        delay={3000}
+        autohide={getAutoHideStatus()}
+        className="banner"
+      >
+        <Toast.Header
+          closeButton={!getAutoHideStatus()}
+          className={getDataCorrespondingStatus().bannerClass}
+         >
+          <i className={`icon ${getDataCorrespondingStatus().iconClass}`} />
+          <strong className="me-auto">{getDataCorrespondingStatus().name}</strong>
+        </Toast.Header>
+        <Toast.Body
+          className={getDataCorrespondingStatus().bannerClass}
+        >
+          {props.message}
+        </Toast.Body>
+      </Toast>
+    </ToastContainer>
+  )
 };
---
+++ ui/src/components/Banner/constants.ts
@@ -0,0 +1,22 @@
+export const BANNER_MESSAGE_STATUSES: any = {
+  SUCCESS: {
+    name: 'Success',
+    bannerClassName: 'banner--status-success',
+    iconClassName: 'icon-good'
+  },
+  WARNING: {
+    name: 'Warning',
+    bannerClassName: 'banner--status-warning',
+    iconClassName: 'icon-warning'
+  },
+  ERROR: {
+    name: 'Error',
+    bannerClassName: 'banner--status-danger',
+    iconClassName: 'icon-critical-stop'
+  },
+  INFO: {
+    name: 'Info',
+    bannerClassName: 'banner--status-information',
+    iconClassName: 'icon-information'
+  }
+};
